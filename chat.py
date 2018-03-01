129eba4283ad9bd1b25592be059e5dfd806bffb0
songbinbin <774663576@qq.com>
2018-03-01 10:25:19 +0800

    保存微信图片

--- chat.py
+++ chat.py
@@ -1,16 +1,26 @@
 import itchat
+
+downloadWePicturePath = 'weimage/'
+
+
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
     return msg.text
+
+
 @itchat.msg_register(itchat.content.PICTURE)
 def picture_reply(msg):
-    return  "不要给我发图片"
+    return "不要给我发图片"
+
+
 @itchat.msg_register(itchat.content.PICTURE)
 def download_files(msg):
-    #msg.download(msg['FileName'])   #这个同样是下载文件的方式
-    msg['Text']('../weimage/'+msg['FileName'])      #下载文件
-    #将下载的文件发送给发送者
-    itchat.send('@%s@%s' % ('img' if msg['Type'] == 'Picture' else 'fil', msg["FileName"]), msg["FromUserName"])
+    # msg.download(msg['FileName'])   #这个同样是下载文件的方式
+    msg['Text'](downloadWePicturePath + msg['FileName'])  # 下载文件
+    # 将下载的文件发送给发送者
+    itchat.send('@%s@%s' % ('img' if msg['Type'] == 'Picture' else 'fil', downloadWePicturePath + msg["FileName"]),
+                msg["FromUserName"])
+
 
 itchat.auto_login()
 itchat.run()
